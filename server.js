3d15198ff42a177bcc465fbf2640aa498a42ac47
Isaie Courilleau <icourilleau@hotmail.fr>
2016-08-05 01:28:37 +0000

    deploy

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 var express = require("express");
 var app = express();
+var multer  = require('multer');
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.post('/', multer({ dest: './uploads/'}).single('upl'), function(req,res){
+	var answer = {size: req.file.size};
+	res.send(answer);
+	res.status(204).end();
+});
 
 var port = process.env.PORT || 8080; // set our port
 
